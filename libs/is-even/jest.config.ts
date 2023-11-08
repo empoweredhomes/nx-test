aa71f11aa1fd448c71b700620db671ed55ccbe40
John Hawley <john@getmysa.com>
2023-11-07 23:28:17 -0330

    Re-initializing is-even project

--- libs/is-even/jest.config.ts
+++ libs/is-even/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
   displayName: 'is-even',
-  preset: '../jest.preset.js',
+  preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../coverage/is-even',
+  coverageDirectory: '../../coverage/libs/is-even',
 };
